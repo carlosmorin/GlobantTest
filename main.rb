d48314c90054488ce6c4e31fbcadcae1ec507bc1
Carlos <j.morin@ukko.mx>
2022-12-21 15:30:11 -0600

    Test units added 3 cases

--- main.rb
+++ main.rb
@@ -1,15 +1,11 @@
 require 'rspec/autorun'
 
 class Calculator
-  def initialize(number)
-    @number = number
-  end
-
-  def reduction
-  	outcome = reduce(@number)
+  def reduction(number)
+  	outcome = add_digits(number)
   	
   	until outcome.to_s.size.eql?(1)
-  	  outcome = reduce(outcome)
+  	  outcome = add_digits(outcome)
   	end
 
   	outcome
@@ -17,17 +13,24 @@ class Calculator
 
   private
 
-  def reduce(num)
+  def add_digits(num)
   	num.to_s.split("").inject(0) { |sum, num| sum += num.to_i }
   end
 end
 
 describe Calculator do
-  it "Calculate" do
-  	calculator = Calculator.new(16)
-  	expect(calculator.reduction).to eql(7)
+  it "Calculate with sort number" do
+    calculator = Calculator.new
+    expect(calculator.reduction(36)).to eql(9)
+  end
+
+  it "Calculate with mid number" do
+    calculator = Calculator.new
+    expect(calculator.reduction(15)).to eql(6)
+  end
+
+  it "Calculate with long number" do
+    calculator = Calculator.new
+    expect(calculator.reduction(205989897)).to eql(3)
   end
 end
-
-calculator = Calculator.new(16)
-puts calculator.reduction
