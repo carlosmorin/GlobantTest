e2f26ececa02443ed33d3470164ff9fe9f49a73f
Carlos <j.morin@ukko.mx>
2022-12-21 14:43:17 -0600

    Test rspec added

--- main.rb
+++ main.rb
@@ -1,3 +1,5 @@
+require 'rspec/autorun'
+
 class Calculator
   def initialize(number)
     @number = number
@@ -9,7 +11,7 @@ class Calculator
   	until outcome.to_s.size.eql?(1)
   	  outcome = reduce(outcome)
   	end
-  	
+
   	outcome
   end
 
@@ -17,6 +19,13 @@ class Calculator
 
   def reduce(num)
   	num.to_s.split("").inject(0) { |sum, num| sum += num.to_i }
+  end
+end
+
+describe Calculator do
+  it "Calculate" do
+  	calculator = Calculator.new(16)
+  	expect(calculator.reduction).to eql(7)
   end
 end
 
