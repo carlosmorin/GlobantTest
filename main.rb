49e1eebc5afedc7c4880d5e3cec0eb5c051d3953
Carlos <j.morin@ukko.mx>
2022-12-21 14:32:38 -0600

    Reductino method and validation

--- main.rb
+++ main.rb
@@ -1,12 +1,24 @@
 class Calculator
   def initialize(number)
-  	@number = number
+    @number = number
   end
 
   def reduction
-  	@number
+  	outcome = reduce(@number)
+  	
+  	until outcome.to_s.size.eql?(1)
+  	  outcome = reduce(outcome)
+  	end
+  	
+  	outcome
+  end
+
+  private
+
+  def reduce(num)
+  	num.to_s.split("").inject(0) { |sum, num| sum += num.to_i }
   end
 end
 
-calculator = Calculator.new(132)
+calculator = Calculator.new(16)
 puts calculator.reduction
