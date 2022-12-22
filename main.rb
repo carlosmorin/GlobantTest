1eb3f54ec32fdce928dcf9c2379c26434782afc6
Carlos <j.morin@ukko.mx>
2022-12-21 20:01:38 -0600

    improve identation

--- main.rb
+++ main.rb
@@ -2,19 +2,19 @@ require 'rspec/autorun'
 
 class Calculator
   def reduction(number)
-  	outcome = add_digits(number)
-  	
-  	until outcome.to_s.size.eql?(1)
-  	  outcome = add_digits(outcome)
-  	end
+    outcome = add_digits(number)
 
-  	outcome
+    until outcome.to_s.size.eql?(1)
+      outcome = add_digits(outcome)
+    end
+
+    outcome
   end
 
   private
 
   def add_digits(num)
-  	num.to_s.split("").inject(0) { |sum, num| sum += num.to_i }
+    num.to_s.split("").inject(0) { |sum, num| sum += num.to_i }
   end
 end
 
